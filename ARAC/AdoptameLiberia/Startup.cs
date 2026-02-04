2579ea4c3c7d63d097c087cd6f4b0d05d8312ed9
Emily094 <acortes.emily@gmail.com>
2026-02-04 15:32:41 -0600

    Usuarios

---
+++ ARAC/AdoptameLiberia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdoptameLiberia.Startup))]
+namespace AdoptameLiberia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
