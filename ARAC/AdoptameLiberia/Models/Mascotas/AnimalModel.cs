e75897b35b052086eeb2616ca95f28852334e33e
brayitan10CR <58745608+brayitan10CR@users.noreply.github.com>
2026-02-05 02:12:45 -0600

    Registro de mascotas

---
+++ ARAC/AdoptameLiberia/Models/Mascotas/AnimalModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdoptameLiberia.Models.Mascotas
+{
+    public class AnimalModel
+    {
+        public int ID_Animal { get; set; }
+        public string Nombre_Animal { get; set; }
+        public int ID_Raza { get; set; }
+        public int ID_TipoAnimal { get; set; }
+        public int? Edad { get; set; }
+        public string Sexo { get; set; }
+        public string Tamano { get; set; }
+        public decimal? Peso { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string NombreRaza { get; set; }
+        public string NombreTipo { get; set; }
+    }
+}
